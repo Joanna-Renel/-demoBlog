c6b545fbc193d1f5ab1e06e405c4661ef6a473d6
Joanna Rénel <joanna.renel@yahoo.fr>
2020-06-30 17:24:58 +0200

    Inscription + Connexion + security.yaml

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,23 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
+// L'objet UniqueEntity permet de préciser les champs qui attendent des données uniques. Ici, le champ "email". Penser à importer la classe.
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *  fields = {"email"},
+ *  message = "Cette adresse email est déjà associée à un compte."
+ * )
  */
-class User
+
+// Pour pouvoir encoder le mot de passe, il faut que notre entité User implemente l'interface UserInterface.
+class User implements UserInterface              
 {
     /**
      * @ORM\Id()
@@ -17,8 +27,13 @@ class User
      */
     private $id;
 
+    //  On appelle l'objet Assert et on pointe sur la contrainte Email pour transformer le champ en type ="email"
+    // fait en sorte l'email que soit unique dans la BDD pour éviter les doublons.
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email(
+     *  message="Cette adresse email '{{ value }}' n'est pas valide."
+     * )
      */
     private $email;
 
@@ -27,10 +42,21 @@ class User
      */
     private $username;
 
+    // On importe l'objet Assert (cf l.7) et on pioche les méthodes Length et EqualTo.
+    // Length est une contrainte de longueur 
+    // EqualTo est une contrainte qui requiert que le champ password soit égal au champ confirm_password
+
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Votre mot de passe doit contenir 8 caractères minimum.")
+     * @Assert\EqualTo(propertyPath="confirm_password", message="Les mots de passe ne correspondent pas.")
      */
     private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Les mots de passe ne correspondent pas.")
+     * 
+     */
 
     // On ajoute une propriété public qui sera en charge de comparer le mot de posse
     // au mot de passe renseigné dans le formulaire.
@@ -77,6 +103,32 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /*
+        Pour pouvoir encoder le mot de passe, il faut que notre entité User implemente l'interface UserInterface.
+        Cette interface contient des méthodes que nous sommes obligés de déclarer :
+        getPassword(), getUsername(), getRoles(), getSalt() et eraseCredentials().    
+    */
+
+    // Cette méthode est uniquement destinée à nettoyer les mots de passe en texte brut éventuellement stockés
+    public function eraseCredentials()
+    {
+
+    }
+
+    // Cette méthode renvoie la chaîne de caractères non encodée que l'utilisateur a saisi et qui, à l'origine,
+    // a été utilisée pour encoder le mot de passe. 
+    public function getSalt()
+    {
+
+    }
+
+    // Cette méthode renvoie un tableau de chaînes de caractères où sont stockés les rôles(droits) accordés à l'utilisateur.
+    // (administrateur ou utilisateur classique)
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
     }
 
 
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 // Class qui permet de générer un formulaire
@@ -14,10 +16,17 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title')
+            // On rajoute un champ qui permettra de sélectionner une catégorie à la création ou à la modification d'un article
+            // EntityType::class précise de quel type de donnée il s'agit. Ici, category est de type "entité"
+            ->add('category', EntityType::class, [
+                    'class' => Category::class,
+                    'choice_label' => 'title' // Le label choisi est le titre de chaque catégorie
+            ])
             ->add('content')
             ->add('image')
             // Champ inutile car dans notre cas, la date se génère automatiquement quand on crée l'article.
             //->add('createdAt')
+            
         ;
     }
 
